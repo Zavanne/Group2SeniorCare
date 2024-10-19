0b025060d2fccb81fa856caab297c0dd04dfcea8
Leivi Dominguez Bruno <165715355+Leivi521@users.noreply.github.com>
2024-10-19 00:16:28 +0000

    signuppage-update

--- src/front/js/pages/patientSignup.js
+++ src/front/js/pages/patientSignup.js
@@ -40,24 +40,30 @@ export const PatientSignUp = () => {
 	};
 
 	return (
-		<div className='text-center mt-5'>
-			<div className='container'>
+		<div
+			style={{
+				backgroundImage: `url("https://images.unsplash.com/photo-1610899881397-7891fad5a1e8?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}
+		>
+
+			<div className='container' style={{ marginLeft: 0, display: "flex", justifyContent: "center", alignItems: "center" }}>
+				<h1 style={{ color: '#0f4c81', textTransform: 'uppercase', fontSize: '2rem' }}>New Patient Signup</h1>
 				{/* Image Section */}
-				<img
-					src='https://i.ibb.co/s3gBtK4/banner-6617553-1280.jpg'
-					alt='Patient'
-					style={{ width: '100%', borderRadius: '10px 10px 0 0' }}
-				/>
 
 				{/* Form Section */}
-				<h1>Patient SignUp</h1>
-				<div className='row d-flex justify-content-center'>
-					<div className='col-6 text-start'>
-						<form onSubmit={handleSubmit}>
-							<div className='form-group my-4'>
-								<label htmlFor='name'>
-									<strong>Full Name</strong>
-								</label>
+				<div className="card-1 p-5" style={{ width: "100%", borderRadius: "0", minWidth: "60rem", fontSize: "1.5rem", backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+					<form onSubmit={handleSubmit} style={{ width: "100%" }}>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='name' className='col-4 col-form-label'>
+								Name
+							</label>
+							<div className='col-8'>
 								<input
 									type='text'
 									id='name'
@@ -66,12 +72,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.name}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='date_of_birth'>
-									<strong>Date of Birth</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='date_of_birth' className='col-4 col-form-label'>
+								Date of Birth
+							</label>
+							<div className='col-8'>
 								<input
 									type='date'
 									id='date_of_birth'
@@ -79,12 +89,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.date_of_birth}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='email'>
-									<strong>Email Address</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='email' className='col-4 col-form-label'>
+								Email Address
+							</label>
+							<div className='col-8'>
 								<input
 									type='email'
 									id='email'
@@ -93,12 +107,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.email}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='password'>
-									<strong>Password</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='password' className='col-4 col-form-label'>
+								Password
+							</label>
+							<div className='col-8'>
 								<input
 									type='password'
 									id='password'
@@ -107,12 +125,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.password}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='city'>
-									<strong>City</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='city' className='col-4 col-form-label'>
+								City
+							</label>
+							<div className='col-8'>
 								<input
 									type='text'
 									id='city'
@@ -121,12 +143,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.city}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='state'>
-									<strong>State</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='state' className='col-4 col-form-label'>
+								State
+							</label>
+							<div className='col-8'>
 								<input
 									type='text'
 									id='state'
@@ -135,12 +161,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.state}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='zipcode'>
-									<strong>Zipcode</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='zipcode' className='col-4 col-form-label'>
+								Zipcode
+							</label>
+							<div className='col-8'>
 								<input
 									type='text'
 									id='zipcode'
@@ -149,12 +179,16 @@ export const PatientSignUp = () => {
 									required
 									value={formData.zipcode}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<div className='form-group my-4'>
-								<label htmlFor='language'>
-									<strong>Language</strong>
-								</label>
+						</div>
+						<div className='form-group mb-4 row'>
+							<label htmlFor='language' className='col-4 col-form-label'>
+								Language
+							</label>
+							<div className='col-8'>
 								<input
 									type='text'
 									id='language'
@@ -163,15 +197,17 @@ export const PatientSignUp = () => {
 									required
 									value={formData.language}
 									onChange={handleChange}
+									className='form-control'
+									style={{ fontSize: "1.5rem" }}
 								/>
 							</div>
-							<button type='submit'>Sign Up</button>
-						</form>
-					</div>
+						</div>
+						<button type='submit' className='btn btn-primary w-100' style={{ fontSize: "1.5rem" }}>Sign Up</button>
+					</form>
 				</div>
+
 			</div>
 		</div>
 	);
 };
-
 
